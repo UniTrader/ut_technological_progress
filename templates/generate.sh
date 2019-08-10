159025aa4b223be54ba19f72329fa2e08cf2aac0
UniTrader <unitrader@gmx.de>
2019-08-11 00:08:25 +0200

    Improvement to xml file generation code now 10000% more generic

--- templates/generate.sh
+++ templates/generate.sh
@@ -2,4 +2,5 @@
 #Generate Turrets and their Bullets
 rm -rf ../assets/generated
 mkdir ../assets/generated
-saxonb-xslt -ext:on -tree:linked -o ../assets/generated/bla.xml turrets.xml turrets.xsl
+saxonb-xslt -ext:on -tree:linked -o ../assets/generated/index.xml turrets.xml macro_gen.xsl
+mv -f ../assets/generated/index.xml ../index/macros.xml
