8cd4ff72ff564963e82257b716b6db16cb799053
UniTrader <unitrader@gmx.de>
2019-08-18 00:17:17 +0200

    update to generation files

--- templates/generate.sh
+++ templates/generate.sh
@@ -3,4 +3,4 @@
 rm -rf ../assets/generated
 mkdir ../assets/generated
 saxonb-xslt -ext:on -tree:linked -o ../assets/generated/index.xml turrets.xml macro_gen.xsl
-mv -f ../assets/generated/index.xml ../index/macros.xml
+saxonb-xslt -ext:on -tree:linked -o ../index/macros.xml turrets.xml index_macros_gen.xsl
